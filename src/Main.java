ff190a368b5c6fceb69a9b383e944f931033cce2
5wixy <104378437+5wixy@users.noreply.github.com>
2023-05-19 22:39:46 +0300

    menu

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,24 @@ public class Main {
             }
         }
     }
+    public static double[] readFileToArray(String dir) {
+        try {
+            String fileContent = Files.readString(Path.of(dir));
+            String[] numbers = fileContent.trim().split("\\s+");
+
+            double[] array = new double[numbers.length];
+            for (int i = 0; i < numbers.length; i++) {
+                array[i] = Double.parseDouble(numbers[i]);
+            }
+            return array;
+        }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        return null;
+
+
+    }
 
 
     public static void makeHeap() throws IOException {
@@ -49,68 +67,74 @@ public class Main {
         System.out.println("Please provide the directory of the input file:");
         Scanner scanner = new Scanner(System.in);
         dir = scanner.next();
-        try {
-            String fileContent = Files.readString(Path.of(dir));
-            String[] numbers = fileContent.trim().split("\\s+");
-
-            double[] array = new double[numbers.length];
-            for (int i = 0; i < numbers.length; i++) {
-                array[i] = Integer.parseInt(numbers[i]);
-            }
+        if (readFileToArray(dir) != null) {
+            double[] array = readFileToArray(dir);
             MinMaxHeap mmh = new MinMaxHeap(array);
             mmh.buildHeap(array);
-            String[] options = {"1- Print Heap",
-                    "2- Extract Max",
-                    "3- Extract Min",
-                    "4- Insert Number",
-                    "5- Delete Number",
-                    "6- Heap Sort",
-                    "7- Exit",
+            menu(mmh, array);
+        }
+    }
 
-            };
-            Scanner menu = new Scanner(System.in);
-            int option = 0;
-            while (option!=7){
-                printMenu(options);
-                try {
-                    option = menu.nextInt();
-                    switch (option){
-                        case 1: System.out.println(Arrays.toString(array));
-                        case 2: array = mmh.heapExtractMax(array); break;
-                        case 3: array = mmh.heapExtractMin(array);break;
-                        case 4: insert(mmh,array);break;
-                        case 5: delete(mmh,array);break;
-                        case 6: heapSort();break;
-                        case 7: exit(0);
-                    }
-                }
-                catch (Exception ex){
-                    System.out.println("Please enter an integer value between 1 and " + options.length);
-                    scanner.next();
-                }
+    public static void menu(MinMaxHeap mmh, double[] array) {
+        String[] options = {"1- Print Heap",
+                "2- Extract Max",
+                "3- Extract Min",
+                "4- Insert Number",
+                "5- Delete Number",
+                "6- Heap Sort",
+                "7- Exit"
+        };
+
+        Scanner menuScanner = new Scanner(System.in);
+        int option = 0;
+        while (option != 7) {
+            printMenu(options);
+            option = menuScanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println(Arrays.toString(array));
+                    break;
+                case 2:
+                    array = mmh.heapExtractMax(array);
+                    break;
+                case 3:
+                    array = mmh.heapExtractMin(array);
+                    break;
+                case 4:
+                    array = insert(mmh, array);
+                    break;
+                case 5:
+                    array = delete(mmh, array);
+                    break;
+                case 6:
+                    heapSort();
+                    break;
+                case 7:
+                    System.exit(0);
             }
         }
-
-    catch(IOException e){
-                e.printStackTrace();
-            }
-
     }
-    public static void insert(MinMaxHeap mmh,double[] array){
+
+    public static double[] insert(MinMaxHeap mmh,double[] array){
         System.out.println("Please enter a number to insert: ");
         Scanner sc = new Scanner(System.in);
         double num = sc.nextDouble();
-        array = mmh.heapInsert(array,num);
+       return mmh.heapInsert(array,num);
 
 
 
 
     }
-    public static void delete(MinMaxHeap mmh, double[] array){
-        System.out.println("Please enter an Integer index to insert: ");
+    public static double[] delete(MinMaxHeap mmh, double[] array){
+        System.out.println("Please enter an Integer index between 0" +" and "+ (array.length-1) + "to delete: ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        array = mmh.heapDelete(array,num);
+        if(num > array.length-1) {
+            delete(mmh,array);
+
+        }
+
+        return mmh.heapDelete(array,num);
 
     }
     public static void heapSort(){
